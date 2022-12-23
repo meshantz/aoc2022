35c6f7524f5906b97c09d34e33566cd1cc0e261c
Mike Shantz <mike.shantz.08@gmail.com>
2022-12-23 15:27:43 -0500

    day 21 part 1 solution and first attempt at part 2

--- src/day21.rs
+++ src/day21.rs
@@ -19,7 +19,16 @@ impl Op {
         }
     }
 
-    fn apply(&self, left: i32, right: i32) -> i32 {
+    fn inverse(&self) -> Op {
+        match self {
+            Op::Add => Op::Sub,
+            Op::Sub => Op::Add,
+            Op::Mul => Op::Div,
+            Op::Div => Op::Mul,
+        }
+    }
+
+    fn apply(&self, left: i64, right: i64) -> i64 {
         match self {
             Op::Add => left + right,
             Op::Sub => left - right,
@@ -31,13 +40,13 @@ impl Op {
 
 #[derive(Debug, Clone)]
 struct ValOrOp {
-    value: Option<i32>,
+    value: Option<i64>,
     operation: Option<Op>,
     variable: Option<String>,
 }
 
 impl ValOrOp {
-    fn as_val(v: i32) -> ValOrOp {
+    fn as_val(v: i64) -> ValOrOp {
         ValOrOp {
             value: Some(v),
             operation: None,
@@ -62,13 +71,68 @@ impl ValOrOp {
     }
 }
 
-pub fn solve() {
-    let raw = fs::read_to_string("data/day21.example").unwrap();
+fn calculate1(part: i32, resolve: &Vec<ValOrOp>) {
+    let mut stack = Vec::new();
+    for step in resolve {
+        if step.value != None {
+            let value = step.value.unwrap();
+            stack.push(value);
+            //println!("Pushed {}", value);
+        } else {
+            let operation = step.operation.unwrap();
+            let right = stack.pop().unwrap();
+            let left = stack.pop().unwrap();
+            //println!("Apply {:?} to {}, {}", operation, left, right);
+            let result = operation.apply(left, right);
+            stack.push(result);
+        }
+    }
+    let final_result = stack.pop().unwrap();
+    println!("Part {}: {}", part, final_result);
+}
+
+fn calculate2(part: i32, resolve: &Vec<ValOrOp>) {
+    let mut stack = Vec::new();
+    let mut alt_stack = Vec::new();
+    for step in resolve {
+        if step.value != None {
+            let value = step.value.unwrap();
+            stack.push(value);
+            //println!("Pushed {}", value);
+        } else {
+            let operation = step.operation.unwrap();
+            let right = stack.pop().unwrap();
+            let left = stack.pop().unwrap();
+            if left == 0 {
+                alt_stack.push(ValOrOp::as_op(operation.inverse()));
+                alt_stack.push(ValOrOp::as_val(right));
+                stack.push(0);
+            } else if right == 0 {
+                alt_stack.push(ValOrOp::as_op(operation.inverse()));
+                alt_stack.push(ValOrOp::as_val(left));
+                stack.push(0);
+            } else {
+                //println!("Apply {:?} to {}, {}", operation, left, right);
+                let result = operation.apply(left, right);
+                if result == 0 {
+                    panic!("Caclulated 0. Invalid assumption")
+                }
+                stack.push(result);
+            }
+        }
+    }
+
+    alt_stack.push(ValOrOp::as_val(0));
+    alt_stack.reverse();
+    calculate1(part, &alt_stack);
+}
+
+fn make_monkeys(raw: &str) -> HashMap<&str, Vec<ValOrOp>> {
     let mut monkeys = HashMap::new();
     for line in raw.lines() {
         let (monkey, operation) = line.split_once(":").unwrap();
         let operation = operation.trim();
-        match operation.parse::<i32>() {
+        match operation.parse::<i64>() {
             Ok(i) => monkeys.insert(monkey, vec![ValOrOp::as_val(i)]),
             Err(err) => {
                 let parts: Vec<&str> = operation.split(" ").collect();
@@ -83,14 +147,22 @@ pub fn solve() {
             }
         };
     }
+    monkeys
+}
 
+fn postfixify(monkeys: &mut HashMap<&str, Vec<ValOrOp>>, keep_human: bool) -> Vec<ValOrOp> {
     let mut resolve = monkeys.remove("root").unwrap();
     let mut index = 0;
-
     while index < resolve.len() {
         let current = resolve[index].clone();
         if current.variable != None {
             let find = current.variable.unwrap().clone();
+            if keep_human && find == "humn" {
+                // 0 is not in the set, maybe we calculate to it though...
+                resolve.splice(index..index + 1, vec![ValOrOp::as_val(0)]);
+                index += 1;
+                continue;
+            }
             let find = find.as_str();
             let insert = monkeys.remove(find).unwrap();
             resolve.splice(index..index + 1, insert);
@@ -98,23 +170,32 @@ pub fn solve() {
             index += 1
         }
     }
+    resolve
+}
 
-    // FIXME: something not right about either the order or the operations...
-    let mut stack = Vec::new();
-    for step in resolve {
-        if step.value != None {
-            let value = step.value.unwrap();
-            stack.push(value);
-            println!("Pushed {}", value);
-        } else {
-            let operation = step.operation.unwrap();
-            let left = stack.pop().unwrap();
-            let right = stack.pop().unwrap();
-            println!("Apply {:?} to {}, {}", operation, left, right);
-            let result = operation.apply(left, right);
-            stack.push(result);
-        }
-    }
-    let final_result = stack.pop().unwrap();
-    println!("Part 1: {}", final_result);
+fn part1(raw: &str) {
+    let mut monkeys = make_monkeys(raw);
+    let resolve = postfixify(&mut monkeys, false);
+    calculate1(1, &resolve);
+}
+
+fn part2(raw: &str) {
+    let mut monkeys = make_monkeys(raw);
+    let root = monkeys.remove("root").unwrap();
+    monkeys.insert(
+        "root",
+        vec![root[0].clone(), root[1].clone(), ValOrOp::as_op(Op::Sub)],
+    );
+    let resolve = postfixify(&mut monkeys, true);
+    calculate2(2, &resolve);
+}
+
+pub fn solve() {
+    let raw = fs::read_to_string("data/day21.example").unwrap();
+    part1(&raw);
+    part2(&raw); // works (301)
+
+    let raw = fs::read_to_string("data/day21.txt").unwrap();
+    part1(&raw);
+    part2(&raw); // incorrect (9626251584895)
 }
