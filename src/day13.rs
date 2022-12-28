2485e87b7b01200faba6bead392a46553ca48b89
Mike Shantz <mike.shantz.08@gmail.com>
2022-12-28 09:20:04 -0500

    day 13 part 2 solution

--- src/day13.rs
+++ src/day13.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{cmp::Ordering, fs};
 
 fn make_packet(val: &str) -> Vec<(i32, i32)> {
     let mut depth = 0;
@@ -59,81 +59,86 @@ fn is_brace(val: i32) -> bool {
     }
 }
 
-fn part1(raw: &str) {
-    let mut sum = 0;
-    for (index, pair_raw) in raw.split("\n\n").enumerate() {
-        let (left, right) = pair_raw.split_once("\n").unwrap();
-        let left = make_packet(left.trim());
-        let right = make_packet(right.trim());
+fn compare(left: &&str, right: &&str) -> Ordering {
+    let left = make_packet(left.trim());
+    let right = make_packet(right.trim());
+    let mut left_stack = left.clone();
+    let mut right_stack = right.clone();
+    let mut is_correct_order: Option<bool> = None;
 
-        let mut left_stack = left.clone();
-        let mut right_stack = right.clone();
-        let mut is_correct_order: Option<bool> = None;
+    left_stack.reverse();
+    right_stack.reverse();
+    // println!("Validating {}", index);
+    while is_correct_order == None {
+        let left_val = left_stack.pop();
+        let right_val = right_stack.pop();
 
-        left_stack.reverse();
-        right_stack.reverse();
-        // println!("Validating {}", index);
-        while is_correct_order == None {
-            let left_val = left_stack.pop();
-            let right_val = right_stack.pop();
+        if left_val == None && right_val == None {
+            panic!("Ran out of input!");
+        } else if left_val == None {
+            is_correct_order = Some(true);
+            continue;
+        } else if right_val == None {
+            is_correct_order = Some(false);
+            continue;
+        }
 
-            if left_val == None && right_val == None {
-                panic!("Ran out of input!");
-            } else if left_val == None {
+        let (left_depth, left_val) = left_val.unwrap();
+        let (right_depth, right_val) = right_val.unwrap();
+        if is_brace(left_val) && is_brace(right_val) && left_depth == right_depth {
+            // both start lists. consume and continue.
+            // println!("two starting parens, consume and continue");
+        } else if is_brace(left_val) && is_brace(right_val) {
+            // one of our lists ended before the other.
+            // println!("one list ended short");
+            if left_depth < right_depth {
                 is_correct_order = Some(true);
-                continue;
-            } else if right_val == None {
+            } else {
                 is_correct_order = Some(false);
-                continue;
             }
-
-            let (left_depth, left_val) = left_val.unwrap();
-            let (right_depth, right_val) = right_val.unwrap();
-            if is_brace(left_val) && is_brace(right_val) && left_depth == right_depth {
-                // both start lists. consume and continue.
-                // println!("two starting parens, consume and continue");
-            } else if is_brace(left_val) && is_brace(right_val) {
+        } else if !is_brace(left_val) && !is_brace(right_val) {
+            // println!("both ints");
+            if left_depth != right_depth {
                 // one of our lists ended before the other.
-                // println!("one list ended short");
+                // println!("one list ended short(2)");
+                // FIXME: too simple. we need to know expected depth of the list...
                 if left_depth < right_depth {
                     is_correct_order = Some(true);
                 } else {
                     is_correct_order = Some(false);
                 }
-            } else if !is_brace(left_val) && !is_brace(right_val) {
-                // println!("both ints");
-                if left_depth != right_depth {
-                    // one of our lists ended before the other.
-                    // println!("one list ended short(2)");
-                    // FIXME: too simple. we need to know expected depth of the list...
-                    if left_depth < right_depth {
-                        is_correct_order = Some(true);
-                    } else {
-                        is_correct_order = Some(false);
-                    }
-                } else if left_val < right_val {
-                    is_correct_order = Some(true);
-                } else if left_val > right_val {
-                    is_correct_order = Some(false);
-                }
-            } else if is_brace(left_val) {
-                // println!("re-push right");
-                left_stack.push((left_depth, left_val));
-                right_stack.push((right_depth + 1, right_val));
-                right_stack.push((right_depth, -1)); // add a brace
-            } else {
-                // println!("re-push left");
-                left_stack.push((left_depth + 1, left_val));
-                left_stack.push((left_depth, -1)); // add a brace
-                right_stack.push((right_depth, right_val));
+            } else if left_val < right_val {
+                is_correct_order = Some(true);
+            } else if left_val > right_val {
+                is_correct_order = Some(false);
             }
+        } else if is_brace(left_val) {
+            // println!("re-push right");
+            left_stack.push((left_depth, left_val));
+            right_stack.push((right_depth + 1, right_val));
+            right_stack.push((right_depth, -1)); // add a brace
+        } else {
+            // println!("re-push left");
+            left_stack.push((left_depth + 1, left_val));
+            left_stack.push((left_depth, -1)); // add a brace
+            right_stack.push((right_depth, right_val));
         }
-        let is_correct_order = is_correct_order.unwrap();
-        // println!("{:?}", left);
-        // println!("{:?}", right);
-        // println!("{:?}", is_correct_order);
-        // println!();
-        if is_correct_order {
+    }
+
+    if is_correct_order.unwrap() {
+        Ordering::Less
+    } else {
+        Ordering::Greater
+    }
+}
+
+fn part1(raw: &str) {
+    let mut sum = 0;
+    for (index, pair_raw) in raw.split("\n\n").enumerate() {
+        let (left, right) = pair_raw.split_once("\n").unwrap();
+        let order = compare(&left, &right);
+
+        if order == Ordering::Less {
             sum += index + 1;
         }
     }
@@ -141,10 +146,37 @@ fn part1(raw: &str) {
     println!("Part 1: {}", sum);
 }
 
+fn part2(raw: &str) {
+    let mut packets = Vec::new();
+    for pair_raw in raw.split("\n\n") {
+        let (left, right) = pair_raw.split_once("\n").unwrap();
+        packets.push(left.trim());
+        packets.push(right.trim());
+    }
+
+    let div_packet1 = "[[2]]";
+    let div_packet2 = "[[6]]";
+    packets.push(div_packet1);
+    packets.push(div_packet2);
+
+    packets.sort_by(compare);
+
+    let mut prod = 1;
+    for (index, p) in packets.iter().enumerate() {
+        if *p == div_packet1 || *p == div_packet2 {
+            prod *= index + 1;
+        }
+    }
+
+    println!("Part 2: {}", prod);
+}
+
 pub fn solve() {
     let raw = fs::read_to_string("data/day13.example").unwrap();
     part1(&raw);
+    part2(&raw);
 
     let raw = fs::read_to_string("data/day13.txt").unwrap();
     part1(&raw);
+    part2(&raw);
 }
