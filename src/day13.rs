0e78b32502624ffc74b9f6dcc3285ce6de067a69
Mike Shantz <mike.shantz.08@gmail.com>
2022-12-28 09:00:55 -0500

    day 13 part 1 solution

--- src/day13.rs
+++ src/day13.rs
@@ -1,105 +1,150 @@
 use std::fs;
 
-#[derive(Debug)]
-enum PacketPartType {
-    List,
-    Int,
-}
-
-#[derive(Debug)]
-struct PacketPart<'a> {
-    kind: PacketPartType,
-    value: &'a str,
-    iter_loc: i32,
-}
-
-#[derive(Debug)]
-struct PacketPair<'a> {
-    first: PacketPart<'a>,
-    second: PacketPart<'a>,
-}
-
-fn new_list(list_str: &str) -> PacketPart {
-    PacketPart {
-        kind: PacketPartType::List,
-        value: list_str,
-        iter_loc: -1,
-    }
-}
-
-fn int_value(part: PacketPart) -> Option<i32> {
-    match part.kind {
-        PacketPartType::Int => Some(part.value.parse().unwrap()),
-        PacketPartType::List => None,
-    }
-}
-
-fn next<'a>(val: &'a mut PacketPart) -> Option<&'a str> {
-    // return the next "list or int"
-    None
-}
-
-fn parens(value: &str) -> Vec<(i32, i32)> {
-    let mut build = Vec::new();
-    let mut stack = Vec::new();
-
-    for (i, c) in value.chars().enumerate() {
-        if c == '[' {
-            stack.push(i as i32);
-        } else if c == ']' {
-            let start = stack.pop().unwrap();
-            build.push((start, i as i32));
-        }
-    }
-
-    build
-}
-
-fn label_parens(val: &str, parens: &Vec<(i32, i32)>) {
-    let mut w = Vec::new();
-    for (i, j) in parens {
-        w.push(*i as usize);
-        w.push(*j as usize);
-    }
-    w.sort();
-    println!("{}", val);
-    let mut k = 0;
+fn make_packet(val: &str) -> Vec<(i32, i32)> {
+    let mut depth = 0;
+    let mut collect = Vec::new();
+    let mut packet = Vec::new();
+    let chars_iter: Vec<char> = val.chars().collect();
     for i in 0..val.len() {
-        if w[k] == i {
-            k += 1;
-            print!("^");
-        } else {
-            print!(" ");
+        let char = chars_iter.get(i);
+        match char {
+            Some('[') => {
+                packet.push((depth, -1));
+                depth += 1;
+            }
+            Some(']') => {
+                // packet.push((depth, -2));
+                depth -= 1;
+            }
+            Some(',') => (), // noop
+            Some(n) => {
+                let next_char = chars_iter.get(i + 1);
+                collect.push(*n);
+                match next_char {
+                    Some('[') => (), // fully collected
+                    Some(']') => (), // fully collected
+                    Some(',') => (), // fully collected
+                    None => panic!("Unexpected end of packet!"),
+                    Some(_) => continue, // more in this number, keep collecting
+                }
+                let integer: i32 = collect.iter().collect::<String>().parse().unwrap();
+                packet.push((depth, integer));
+                collect.clear();
+            }
+            None => (),
         }
     }
-    println!();
+    packet
+}
+
+// fn is_open_brace(val: i32) -> bool {
+//     match val {
+//         -1 => true,
+//         _ => false,
+//     }
+// }
+
+// fn is_close_brace(val: i32) -> bool {
+//     match val {
+//         -2 => true,
+//         _ => false,
+//     }
+// }
+
+fn is_brace(val: i32) -> bool {
+    match val {
+        -1 => true,
+        // -2 => true,
+        _ => false,
+    }
+}
+
+fn part1(raw: &str) {
+    let mut sum = 0;
+    for (index, pair_raw) in raw.split("\n\n").enumerate() {
+        let (left, right) = pair_raw.split_once("\n").unwrap();
+        let left = make_packet(left.trim());
+        let right = make_packet(right.trim());
+
+        let mut left_stack = left.clone();
+        let mut right_stack = right.clone();
+        let mut is_correct_order: Option<bool> = None;
+
+        left_stack.reverse();
+        right_stack.reverse();
+        // println!("Validating {}", index);
+        while is_correct_order == None {
+            let left_val = left_stack.pop();
+            let right_val = right_stack.pop();
+
+            if left_val == None && right_val == None {
+                panic!("Ran out of input!");
+            } else if left_val == None {
+                is_correct_order = Some(true);
+                continue;
+            } else if right_val == None {
+                is_correct_order = Some(false);
+                continue;
+            }
+
+            let (left_depth, left_val) = left_val.unwrap();
+            let (right_depth, right_val) = right_val.unwrap();
+            if is_brace(left_val) && is_brace(right_val) && left_depth == right_depth {
+                // both start lists. consume and continue.
+                // println!("two starting parens, consume and continue");
+            } else if is_brace(left_val) && is_brace(right_val) {
+                // one of our lists ended before the other.
+                // println!("one list ended short");
+                if left_depth < right_depth {
+                    is_correct_order = Some(true);
+                } else {
+                    is_correct_order = Some(false);
+                }
+            } else if !is_brace(left_val) && !is_brace(right_val) {
+                // println!("both ints");
+                if left_depth != right_depth {
+                    // one of our lists ended before the other.
+                    // println!("one list ended short(2)");
+                    // FIXME: too simple. we need to know expected depth of the list...
+                    if left_depth < right_depth {
+                        is_correct_order = Some(true);
+                    } else {
+                        is_correct_order = Some(false);
+                    }
+                } else if left_val < right_val {
+                    is_correct_order = Some(true);
+                } else if left_val > right_val {
+                    is_correct_order = Some(false);
+                }
+            } else if is_brace(left_val) {
+                // println!("re-push right");
+                left_stack.push((left_depth, left_val));
+                right_stack.push((right_depth + 1, right_val));
+                right_stack.push((right_depth, -1)); // add a brace
+            } else {
+                // println!("re-push left");
+                left_stack.push((left_depth + 1, left_val));
+                left_stack.push((left_depth, -1)); // add a brace
+                right_stack.push((right_depth, right_val));
+            }
+        }
+        let is_correct_order = is_correct_order.unwrap();
+        // println!("{:?}", left);
+        // println!("{:?}", right);
+        // println!("{:?}", is_correct_order);
+        // println!();
+        if is_correct_order {
+            sum += index + 1;
+        }
+    }
+
+    println!("Part 1: {}", sum);
 }
 
 pub fn solve() {
     let raw = fs::read_to_string("data/day13.example").unwrap();
-    let mut packets = Vec::new();
+    part1(&raw);
 
-    for pair_raw in raw.split("\n\n") {
-        let parts: Vec<&str> = pair_raw.split("\n").collect();
-        packets.push(PacketPair {
-            first: new_list(parts[0]),
-            second: new_list(parts[1]),
-        })
-    }
-
-    // for packet in packets {
-    //     println!("{:?}", packet)
-    // }
-    let test_packet = &packets[7].first;
-    let v = parens(test_packet.value);
-    label_parens(test_packet.value, &v);
-
-    println!(
-        "{:?}",
-        int_value(PacketPart {
-            kind: PacketPartType::Int,
-            value: "23",
-            iter_loc: -1,
-        })
-    )
+    let raw = fs::read_to_string("data/day13.txt").unwrap();
+    part1(&raw);
 }
